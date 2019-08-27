69a12918c2a1b8600e46ccbc5ace7ad823aa8287
motomotoyoshi <motomoto.git@gmail.com>
2019-08-28 00:50:21 +0900

    update, main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@
         constructor() {
             this.el = document.createElement('li');
             this.el.classList.add('pressed');
+            this.el.addEventListener('click', () => {
+                this.check();
+            });
         }
 
         getEl() {
@@ -14,6 +17,13 @@
         activate(num) {
             this.el.classList.remove('pressed');
             this.el.textContent = num;
+        }
+
+        check() {
+            if (currentNum === parseInt(this.el.textContent, 10)) {
+                this.el.classList.add('pressed');
+                currentNum++;
+            }
         }
     }
     class Board {
@@ -33,13 +43,18 @@
         }
 
         activate() {
+            const nums = [0, 1, 2, 3];
+
             this.panels.forEach(panel => {
-                panel.activate(0);
+                const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+                panel.activate(num);
             });
         }
     }
 
     const board = new Board();
+
+    let currentNum = 0;
 
     const btn = document.getElementById('btn');
     btn.addEventListener('click', () => {
